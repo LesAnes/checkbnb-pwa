fccebf5de924887e9cc5eccde22c8041a6f912a3
Aymeric Dominique <aymeric.dominique@gmail.com>
2025-02-27 14:42:01 +0100

    version

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <v-app>
     <router-view />
+    <span class="position-absolute bottom-0 right-0 ma-2">
+      {{ version }}
+    </span>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+  const version = ref('v1.0.4')
 </script>
